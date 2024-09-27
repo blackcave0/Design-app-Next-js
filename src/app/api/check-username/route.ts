5be2c6cd5c9e9c9caab66c86694b7d4e73192cc3
sarvesh.sri00 <sarvesh.sri00@gmail.com>
2024-09-27 21:38:35 +0000

    checking unique username using zod and database and query params

---
+++ src/app/api/check-username/route.ts
@@ -0,0 +1,54 @@
+//checking unique username 
+import dbConncet from '@/lib/dbConnect'
+import UserModel from '@/model/User'
+import {z} from 'zod'
+import { userNameValidation } from '@/schemas/signUpSchema'
+
+const UserNameQuerySchema = z.object({
+    username: userNameValidation
+})
+
+export async function GET(request: Request){
+  await dbConncet();
+  try {
+    //checking username by url
+    const {searchParams} = new URL(request.url);
+    const qeuryParam = {username : searchParams.get('username')};
+
+    // validate with zod
+    const result = UserNameQuerySchema.safeParse(qeuryParam);
+    console.log(result) //::todo Remove
+    if(!result.success){
+      const usernameError = result.error.format().username?._errors || [];
+      return Response.json({
+        success : false,
+        message : usernameError.length > 0 ? usernameError.join(', ') : 'Invalid username'
+      });
+    };
+
+    //destructure username from result.data
+    const {username} = result.data;
+
+    //finding username in database which is already verified
+    const existingVerifiedUser = await UserModel.findOne({username, isVerified : true}) 
+    if(existingVerifiedUser){
+      return Response.json({
+        // if username exist then return success false
+        success : false,
+        message : 'Username already taken'
+      }, {status : 409})
+    };
+
+    return Response.json({
+      success : true,
+      message : 'Username available'
+    }, {status : 200})
+    
+  } catch (error) {
+    console.log('Error checking username', error);
+    return Response.json({
+      success: false,
+      message: 'Error checking username'
+    }, {status : 500})
+  }
+}
